f0be5dcb63213e4f3eafd1a56c98e598b0a677bd
free <datelvudy03@seznam.cz>
2021-04-20 09:43:00 -0700

    1 working :D

---
+++ src/main/java/com/eshop/javadu/JavaDuApplication.java
@@ -0,0 +1,14 @@
+package com.eshop.javadu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class JavaDuApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaDuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eshop/javadu/JavaDuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eshop.javadu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
